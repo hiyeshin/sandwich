2003b684c9b8db02d50355a060c79d0c4f26922f
Hiye Shin <hiyeshin@gmail.com>
2012-10-08 19:39:08 -0400

    started working

--- models.py
+++ models.py
@@ -15,21 +15,19 @@ class Comment(EmbeddedDocument):
 
 class Sandwich(Document):
 	
-	creator = StringField(max_length=120, required=True, verbose_name="Name", help_text="What's your name?")
-	title = StringField(max_length=120,required=True)
-	slug=StringField()
-	#I would not have the 'idea' category
-	
-	#Category is a list of Strings
+	creator = StringField(max_length=120, required=True, verbose_name="First name", help_text="Please enter your first name")
+	title = StringField(max_length=120, required=True)
+	slug = StringField()
+	sandwich = StringField(required=True, verbose_name="What is your sandwich?")
+
+	# Category is a list of Strings
 	bread = ListField(StringField(max_length=30))
-	flavors = ListField(StringField(max_length = 30))
-	cheese = ListField(StringField(max_length = 30))
-	veggies = ListField(StringField(max_length = 30))
-	spread = ListField(StringField(max_length = 30))
-	
-	#comments is a list of Document type defined above
-	comments = ListField(EmbeddedDocumentField(Comment))
-	
+
+	# Comments is a list of Document type 'Comments' defined above
+	comments = ListField( EmbeddedDocumentField(Comment) )
+
+	# Timestamp will record the date and time idea was created.
 	timestamp = DateTimeField(default=datetime.now())
+
 	
 SandwichForm = model_form(Sandwich)
